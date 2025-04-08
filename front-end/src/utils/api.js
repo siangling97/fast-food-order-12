a803d3706a7063a51bc8a7098e4545aba8d70305
Siang Ling <siangling@Siangs-MacBook-Pro.local>
2025-04-08 17:13:53 -0500

    Fix API_BASE_URL to use env only

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,8 +1,8 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-time"; // Corrected import
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://restaurant-reservation-1-duek.onrender.com";
+
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 /**
  * Defines the default headers for these functions to work with `json-server`
